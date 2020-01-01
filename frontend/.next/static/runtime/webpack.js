89218a2a2aa6283b7d8facbbc7b39ab35e85de25
dkang417 <dkang417@gmail.com>
2020-01-01 14:03:37 -0500

    update nprogress

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e9c51b022f04169842ac";
+/******/ 	var hotCurrentHash = "8f5118bb5c39a97cf737";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
