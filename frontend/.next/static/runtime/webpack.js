3c8860c0b41b4d26f51b6a8d762ebab759ca28a6
dkang417 <dkang417@gmail.com>
2020-01-02 20:21:16 -0500

    _document

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8f5118bb5c39a97cf737";
+/******/ 	var hotCurrentHash = "f29167abadea3548d65c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
